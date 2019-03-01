f1fbf304dd3a6777000a490377c2b95cc3c91e41
sampraslee <sampras.lee8@gmail.com>
2019-03-01 16:13:56 +0800

    Scrapped the following data from BPFK website, registered, unregistered and cosmetic products. Slight problem with cosmetics that will require Regex to solve.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_28_011404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.string "chemical"
+    t.integer "product_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
